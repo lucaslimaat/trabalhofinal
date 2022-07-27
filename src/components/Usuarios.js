e2a1d661fe1c357e2faf6b907a42da3f45e2c74a
Lucas Lima Torquato <82185051+lucaslimaat@users.noreply.github.com>
2022-07-27 12:35:41 -0300

    asdfsd

--- src/components/Usuarios.js
+++ src/components/Usuarios.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 
 
+
 export default function Usuarios() {
   const [produtos, setProdutos] = useState([]);
   
@@ -33,13 +34,16 @@ export default function Usuarios() {
           console.log(item)
             return (
               <div key={item.id}>
-                <Teste>
+               <BodyProdutos>
+               <Teste>
                   
-                  {" "}
+                  
                   <Imagens src={`img/${item.img}`} alt="imgcamisa"/>
-                  - <Span>{item.nome_produto}</Span> - <Span>{item.tamanhos_disp}{" "}</Span>
-             
-                </Teste>
+                  <Span>{item.nome_produto}</Span>  
+                  <Span>Tamanhos Disponíveis: {item.tamanhos_disp}{" "}</Span>
+                 
+                 </Teste>
+                </BodyProdutos> 
               </div>
             );
           })
@@ -58,15 +62,29 @@ justify-content: space-between;
 align-items: center;
 background-color: white;
 `
+const BodyProdutos = styled.div`
+display: flex;
+justify-content: space-evenly;
+margin-top: 20px;
+margin-bottom: 20px;
+align-items: center;
+height: auto;
+width: 100%;
+background: white;
+flex-wrap: wrap;
+`
+
 const Teste = styled.div`
 background-color: white;
 display: flex;
 flex-direction: column;
+width: 100%;
 `
 
 
 const Span = styled.span`
-color: yellow;
+color: black;
+text-align: center;
 `
 const Body1 = styled.div`
 width: 100%;
@@ -79,16 +97,27 @@ background-color: white;
 `
 
 const Body3 = styled.div`
-
+width: 100%;
 height: auto;
 display: flex;
-flex-direction: column;
-justify-content: space-between;
+flex-direction: row;
+justify-content: space-evenly;
 align-items: flex-start;
 background-color: white;
 text-align: left;
+flex-wrap: wrap;
 `
 const Imagens = styled.img`
-height: 500px;
-width: 500px;
+width: 400px;
+height: 400px;
+border-width: 3px;
+border-color: black;
+border-style: solid;
+border-radius: 16px;
+cursor: pointer;
+:hover{
+  border-color: #D3D3D3;
+  transition: 0.3s;
 `
+
+
