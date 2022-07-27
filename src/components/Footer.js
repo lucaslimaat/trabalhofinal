94f82e6e8241cdb90495b277a0f147a3062a74ec
Lucas Lima Torquato <82185051+lucaslimaat@users.noreply.github.com>
2022-07-27 12:34:19 -0300

    Mudanças

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,10 +9,9 @@ const Footer = () => {
             <BodyRodape>
             <Title>Outer</Title>
             <Text>A Outer é uma loja de roupas online que surgiu em 2022, contendo roupas e artigos de moda. Com marcas como: Nike, Adidas, Puma, Lacoste, New Era, nosso estoque oferece o melhor serviço possivel. Nossa venture já nasceu cheia de expectativas e possibilidades, renovando-as mês a mês, sempre somando qualidade aos produtos que oferece à seus clientes.</Text>
-            </BodyRodape>
-            <Container>
             <Copy>Copyright &copy;2022 Outer Loja Oficial</Copy>
-            </Container>
+            </BodyRodape>
+            
         </Body>
 
     )
@@ -54,18 +53,12 @@ text-align: center;
  font-size: 15px;
 }
 `
-const Container = styled.div`
-    background: #EFEFEF;
-    width: 97%;
-    height: auto;
-    padding: 0 20px;
-    text-align: right;
-`
+
 const Copy = styled.div`
 font-size: 14px;
 word-spacing: 2px;
 font-family: Josefin Sans, Arial, Helvetica, sans-serif;
-margin-right: 10px;
+
 `
 
 export default Footer
